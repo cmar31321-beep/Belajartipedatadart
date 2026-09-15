4fbc0afb11904b4fd86c3b29b33e2db8c920656b
cmar31321-beep <cmar31321@gmail.com>
2026-09-15 22:27:39 +0700

    versi3

--- latihan.dart
+++ latihan.dart
@@ -34,3 +34,35 @@ void main(){
   // //datamhs.elementAt() datamhs[]
   // print('saya ${datamhs.indexof(10)}');
 //}
+
+void main() {
+  // set
+  // Set namamhs = {'A','B','C','B','A','D'};
+  // //print ('nama saya ${nmmhs[2]}');
+  // print('nama saya ${namamhs.elementAt(2)}');
+
+  // // map
+  // Map<String,String> datamhs ={'nama': 'Deka','berat': '30'};
+  // print('nama saya ${datamhs['nama']},berat saya ${datamhs['berat']}');
+
+  // var datamhs=['A','B',0];
+  // var datamhs1={'A','B',0};
+  // var datamhs2={'nama': 'Deka', 'berat':30};
+
+  //final vs const
+
+  // final String nama;
+  // nama = 'Cindy';
+  // const String nama1='Sindy';
+  //const hanya dapat di masukkan saat deklarasi sedangkan final tetap bisa di masukkan setelah deklarasi namun hanya 1x
+
+  // Map data1  = {"nama":"Simon", "Umur": 28,"prodi":"BD"};
+  // Map data2  = {"nama":"Rafael", "Umur": 25,"prodi":"SI"};
+  // Map data3  = {"nama":"Zuan", "Umur": 22,"prodi":"TI"};
+
+  // List data =[];
+  // data.add(data1);
+  // data.add(data2);
+  // data.add(data3);
+
+  // print(data);
