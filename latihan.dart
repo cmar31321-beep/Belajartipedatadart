6b5976aeea2a1ee08273164aa9b05b2041670e87
cmar31321-beep <cmar31321@gmail.com>
2026-09-15 22:18:39 +0700

    versuin1

---
+++ latihan.dart
@@ -0,0 +1,11 @@
+void main(){
+  String nama = 'Cindy';
+  int umur =20;
+  double beratbadan =56.5;
+
+  //saya cindy,umur saya 20 tahun dan berat badan saya 56.5 kg
+  print('saya $nama,umur saya $umur tahun, berat badan saya $beratbadan kg');
+
+ //string, double,num,int, bool
+ //List,set,map
+ //var, dynamic
