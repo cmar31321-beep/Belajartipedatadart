dde9538b3e14fce1ba1c3fe2a19c3b8a548ae149
cmar31321-beep <cmar31321@gmail.com>
2026-09-15 22:25:03 +0700

    versi2

--- latihan.dart
+++ latihan.dart
@@ -9,3 +9,28 @@ void main(){
  //string, double,num,int, bool
  //List,set,map
  //var, dynamic
+
+ //  int angka1 =100;
+//  double angka2 = 20;
+//  num hasil = 20;
+//  print(angka2);
+//  print(hasil);
+ //  var nilai1=100;
+//  dynamic nilai2=100;
+//  var nilai3=true;
+//  dynamic nilai=true;
+  // var nilai1=100;
+  // dynamic nilai2=100;
+  // var nilai3 = true;
+  // dynamic nilai4=true;
+
+// }
+  //list => array dengan index int
+  // List datamhs =[];
+  // //print ('saya pilih ${datamhs[2]}');
+  // datamhs.add('A');
+  // datamhs.add('true');
+  // datamhs.add(10);
+  // //datamhs.elementAt() datamhs[]
+  // print('saya ${datamhs.indexof(10)}');
+//}
